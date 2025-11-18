2ba945eb6bc2152c00eb02201c6ff507b135100f
Xuan Son <buixu4ns0n@gmail.com>
2025-11-19 02:41:26 +0700

    router

--- src/views/Nav/nav.js
+++ src/views/Nav/nav.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './nav.scss';
-import { NavLink, Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 
 class Nav extends React.Component {
   render() {
